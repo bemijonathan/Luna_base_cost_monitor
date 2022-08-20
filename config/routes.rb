0af869cf4d95f70f6c76440f75afbc1051143954
Jonathan Atiene <34762800+bemijonathan@users.noreply.github.com>
2022-08-20 12:03:30 +0100

    new begining

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :items
-  post "sign_up" => 'user_creation#create'
-  get "sign_out" => 'user_creation#destroy'
-  post "login_in" => 'user_creation#login'
-  get "workers" => "user_creation#all"
-end
